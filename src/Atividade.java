7698317687fc2837294148120e7369486477a368
Emilly <eoliveiraa2007@gmail.com>
2025-09-02 19:48:49 -0300

    feat: Trabalhando POO e records

--- src/Atividade.java
+++ src/Atividade.java
@@ -1,16 +1,10 @@
-import java.util.Scanner;
-
 public class Atividade {
-
     public static void main(String[] args) {
+        var male = new Person("João");
+        male.setAge(12);
 
-        var male = new Person();
-        male.name = "João";
-        male.age = 12;
-
-        var female = new Person();
-        female.name = "Maria";
-        female.age = 20;
+        var female = new Person("Maria");
+        female.setAge(20);
 
         System.out.println("Male name: " + male.getName() + ", age: " + male.getAge());
         System.out.println("Female name: " + female.getName() + ", age: " + female.getAge());
--- src/Person.java
+++ src/Person.java
@@ -1,15 +1,34 @@
 public class Person {
-
     private String name;
-
-
     private int age;
 
-    public String getName(){
-        return name;
+    public Person() {}
+
+    public Person(String name) {
+        this.name = name;
     }
 
-    public int getAge(){
-        return age;
+    // Getters
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
+
+//Records
+/// public record Person(String name, int age){}
+//Eles são imutáveis por padrão, o que significa que não podemos alterar seus valores
+// após a criação do objeto. Além disso, eles são bastante úteis para representar dados,
+// como, por exemplo, uma entidade de banco de dados
